f0d1907b436f4ef2a3252a5beec7ec2afc6628df
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-10-11 19:53:58 +0300

    ;lesson 12:)

---
+++ PY projects/Урок2.py
@@ -0,0 +1,14 @@
+# a = int(input(" Введите число  a "))
+# b = int(input(" Введите число  b "))
+# while a <= b:
+#     print(a, end=" ")
+#     a += 1
+
+
+n = int(input(" Введите число n "))
+sum = 0
+i = 1
+while i <= n:
+    sum += i
+    i += 1
+print(sum)
